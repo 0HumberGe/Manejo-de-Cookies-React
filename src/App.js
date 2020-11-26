37329e133aa62448cd491c6c9f9600094a1cbc16
Humberto <hgerardop01@gmail.com>
2020-11-26 11:09:51 -0600

    State Updated
    El estado del componente se actualiza con los datos ingresados en el form. Crear las cookies  y mostrarlas pendiente

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import './App.css';
 
+import Inicio from './component/Inicio';
+
 function App() {
   return (
-    <div className="App-header">
-      <p className="mayus">formulario simple para la clase de lenguajes modernos de programación</p>
-        <form>
-
-          <div className="field">
-            <label className="label">Nombre</label>
-            <div className="control">
-              <input className="input" type="text" placeholder="Nombre" />
-            </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Matricula</label>
-            <div className="control">
-              <input className="input" type="text" placeholder="Matricula" />
-            </div>
-          </div>
-
-          <div className="field">
-            <label className="label">Clase</label>
-            <div className="control">
-              <input className="input" type="text" placeholder="Clase" />
-            </div>
-          </div>
-          
-        </form>
-        <div className="field is-grouped">
-          <div className="control">
-            <button className="button is-link">Submit</button>
-          </div>
-        </div>
-    </div>
+    <Inicio/>
   );
 }
 
